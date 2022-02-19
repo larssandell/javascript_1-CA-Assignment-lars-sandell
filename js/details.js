595fc9573595c574d9f5f42ff1167143f71d70e3
larssandell <91052943+larssandell@users.noreply.github.com>
2022-02-19 11:54:07 +0100

    loading og form
    fikset loader.
    lagt til form i cantact.html

--- js/details.js
+++ js/details.js
@@ -6,26 +6,30 @@ const id = params.get("id");
 
 const urlApi = "https://rickandmortyapi.com/api/character/" + id;
 const main = document.querySelector("main")
-const char = document.querySelector(".char")
-
+const charContainer = document.querySelector(".char-container")
 
 async function getCharacters() {
     try {
         const response = await fetch(urlApi);
         const apiChar = await response.json();
+        document.querySelector(".loader").innerHTML = "";
         createHtml(apiChar);
 
     }catch(err) {
         console.log("What went wrong", err);
-        main.innerHtml = err;
+        main.innerHtml = `<div class="error"><img src="/images/error404.png></img></div>`;
     }
 }
 
 getCharacters();
 
 function createHtml(apiChar) {
-    char.innerHTML = `<div class="char"><h1>Name: ${apiChar.name}</h1>
-                      <h2>Gender: ${apiChar.gender}</h2>
+    console.log(apiChar);
+    charContainer.innerHTML = `<div class="char"><h1>${apiChar.name}</h1>
+                      <img src="${apiChar.image}"></img></div>
+                      <div class="char"><h2>Gender: ${apiChar.gender}</h2>
                       <h2>Species: ${apiChar.species}</h2>
-                      <img class="avatar">${apiChar.image}</img></div>`
+                      <h2>Location: ${apiChar.location.name}</h2>
+                      <h2>Status: ${apiChar.status}</h2></div>
+                      <div><ul>Episodes: <a>${apiChar.episode}</a></ul></div>`
 }
--- js/script.js
+++ js/script.js
@@ -7,32 +7,24 @@ const section = document.querySelector("section")
 const loader = document.querySelector(".loader");
 
 
-
 async function callApi() {
     try {
         const response = await fetch(urlApi)
         const data = await response.json();
         const getArray = data.results;
         console.log(getArray);
-        document.querySelector(".loader").innerHTML = "";
+        loader.innerHTML = "";
         list.innerHTML = "";
         getArray.forEach(function(card) {
-            list.innerHTML += `<li><a href="details.html?id=${card.id}">
-                                    <p>Character: ${card.name}</p></li>`
+            list.innerHTML += `<div><li><a href="details.html?id=${card.id}">${card.name}</li></div>`
         });
     }catch(err) {
         console.log("Her er feilen " ,err);
-        loader.innerHTML = "";
+        loader.innerHTML = "none";
+        document.querySelector("main").innerHTML = `<div class="error">
+                                                        <img src="/images/error404.png"></a>
+                                                        <h2>${err}</h2>
+                                                    <div>`;
     }
 }
 callApi();
-
-
-// function showList(event) {
-//     console.log(event.target.event);
-//     const list = event.target.event;
-//     for(let i = 0; i < list; i++) {
-//         list.innerHtml = `<a class="list" href="details.html?id=${i}">${i}</a>`;
-//     }
-// }
-// showList();
