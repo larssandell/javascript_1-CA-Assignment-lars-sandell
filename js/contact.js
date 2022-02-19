cb3803c0ff83e145cfabde30e23669e0f31e9c2d
larssandell <91052943+larssandell@users.noreply.github.com>
2022-02-19 13:37:57 +0100

    contact
    added label and starting with script

---
+++ js/contact.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("#contact-form");
+
+form.onsubmit = function(e) {
+    e.preventDefault();
+    const name = document.querySelector("#name");
+    const subject = document.querySelector("#subject");
+    const email = document.querySelector("#email");
+
+}
+
+function emailValidation(){
+}
