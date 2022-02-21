d8904498c11cfa0b408bd7ec625c351fc9071d8a
larssandell <91052943+larssandell@users.noreply.github.com>
2022-02-21 16:01:52 +0100

    form

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,52 @@
 const form = document.querySelector("#contact-form");
+const formUncomplete = document.querySelector(".form-uncomplete")
+const fullName = document.querySelector("#name");
+const nameErr = document.querySelector("#err-name");
+const subject = document.querySelector("#subject");
+const subjectErr = document.querySelector("#err-sub");
+const email = document.querySelector("#email");
+const emailErr = document.querySelector("#err-email");
 
-form.onsubmit = function(e) {
-    e.preventDefault();
-    const name = document.querySelector("#name");
-    const subject = document.querySelector("#subject");
-    const email = document.querySelector("#email");
 
+function valiForm(event) {
+    event.preventDefault();
+    if (checkForm(fullName.value, 1)) {
+        nameErr.style.display = "none";
+    } else {
+        nameErr.style.display = "block";
+    }
+    if (checkForm(subject.value, 10)) {
+        subjectErr.style.display = "none";
+    } else {
+        subjectErr.style.display = "block";
+    }
+    if (emailValidation(email.value)) {
+        emailErr.style.display = "none";
+    } else {
+        emailErr.style.display = "block";
+    }
+    // level 2 når sendt ok 
+    //return formUncomplete.innerHTML = `<h2>The form was sucessfuly sent</h2>`
 }
 
-function emailValidation(){
+form.addEventListener("submit", valiForm);
+
+function checkForm(value, leng) {
+    if (value.trim().length >= leng) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+function emailValidation(email){
+    const regEx = /\S+@\S+\.\S+/;
+    const emailMatche = regEx.test(email);
+    return emailMatche;
+}
+
+
+
+//email - minimum length of 25
+// subject - minimum length of 10
+// name - required
