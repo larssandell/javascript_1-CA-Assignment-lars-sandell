fb5741757bdd6df2c2e5d193aa06782e5166b988
larssandell <91052943+larssandell@users.noreply.github.com>
2022-02-24 10:01:06 +0100

    css update

--- js/details.js
+++ js/details.js
@@ -28,7 +28,7 @@ function createHtml(apiChar) {
     document.title = `${apiChar.name}`
     charContainer.innerHTML = `<div class="char"><h1>${apiChar.name}</h1>
                       <img src="${apiChar.image}"></img></div>
-                      <div class="char"><h3>Gender:<h3><h3 class="h3-blue">${apiChar.gender}</h3>
+                      <div class="char-info"><h3>Gender:<h3><h3 class="h3-blue">${apiChar.gender}</h3>
                       <h3>Species:</h3><h3 class="h3-blue">${apiChar.species}</h3>
                       <h3>Location:</h3><h3 class="h3-blue">${apiChar.location.name}</h3>
                       <h3>Status:</h3><h3 class="h3-blue">${apiChar.status}</h3><h3>Number of episodes:</h3><h3 class="h3-blue">${apiChar.episode.length}</h3></div>`
